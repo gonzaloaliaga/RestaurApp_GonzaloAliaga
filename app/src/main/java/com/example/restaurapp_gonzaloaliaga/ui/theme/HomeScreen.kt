969ff4c82a79256e1c28607025548b76e5de270e
PV-Alumno <PV-Alumno@duoc.acad>
2025-10-03 18:31:12 -0300

    pruebas

--- app/src/main/java/com/example/restaurapp_gonzaloaliaga/ui/theme/HomeScreen.kt
+++ app/src/main/java/com/example/restaurapp_gonzaloaliaga/ui/theme/HomeScreen.kt
@@ -1,5 +1,7 @@
-package com.example.restorinijapixini_joseretamal.ui.theme
+package com.example.restaurapp_gonzaloaliaga.ui.theme
+import com.example.restaurapp_gonzaloaliaga.R
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.Image
 import androidx.compose.material3.Button
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Text
@@ -9,6 +11,9 @@ import androidx.compose.ui.unit.dp
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.TopAppBar
+import androidx.compose.ui.res.painterResource
+
+
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun HomeScreen() {
@@ -45,4 +50,29 @@ fun HomeScreen() {
 @Composable
 fun HomeScreenPreview() {
     HomeScreen()
+}
+
+// Pruebas
+
+data class Message(val author: String, val body: String)
+@Composable
+fun MessageCard(msg: Message) {
+    Row {
+        Image(
+            painter = painterResource(R.drawable.pear),
+            contentDescription = "Contact profile picture"
+        )
+        Column {
+            Text(text = msg.author)
+            Text(text = msg.body)
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun PrevewMessageCard(){
+    MessageCard(
+        msg = Message("Lexi", "Hey, take a look at jetpack compose")
+    )
 }
